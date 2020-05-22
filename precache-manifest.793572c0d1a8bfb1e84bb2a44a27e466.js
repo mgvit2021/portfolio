04bed19d0c3f448006e8435efbcf9a2ee58cba34
Mridul Gupta <mgvit2021@gmail.com>
2020-05-23 03:59:39 +0530

    Updates

---
+++ precache-manifest.793572c0d1a8bfb1e84bb2a44a27e466.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b262f03c71072d36ff691a382a1f5d12",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2537f03002a9e2235096",
+    "url": "/portfolio/static/css/main.dd6a92e0.chunk.css"
+  },
+  {
+    "revision": "648f1aa61f3acd2b8c5b",
+    "url": "/portfolio/static/js/2.12f50ea1.chunk.js"
+  },
+  {
+    "revision": "92ba8acd1e9fee970a07db04009ca21d",
+    "url": "/portfolio/static/js/2.12f50ea1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2537f03002a9e2235096",
+    "url": "/portfolio/static/js/main.36b9c2b0.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "742ab9985a526bde98e3d240f58e00ea",
+    "url": "/portfolio/static/media/Cloak.742ab998.gif"
+  },
+  {
+    "revision": "a113cd9c914665fe5020a75aae9cdbb0",
+    "url": "/portfolio/static/media/Course.a113cd9c.jpg"
+  },
+  {
+    "revision": "c50354959729c363d2a1b20f710e2837",
+    "url": "/portfolio/static/media/PMS.c5035495.svg"
+  },
+  {
+    "revision": "2f1535ee0281e4c966d40c093144ed50",
+    "url": "/portfolio/static/media/SIHImage.2f1535ee.jpeg"
+  },
+  {
+    "revision": "a5fdbb70d649fc0e32bd33a76ffcfd08",
+    "url": "/portfolio/static/media/Taxi.a5fdbb70.png"
+  }
+]);
